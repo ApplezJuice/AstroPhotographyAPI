ec2c755bda33259ac7cf5f1c977b0572d42aca7f
Michael Hayes <mhayes@blizzard.com>
2019-12-22 20:09:21 -0800

    Can generate transparency information now

--- AstroPhotographyAPI/Controllers/AstroPhotographyAPIController.cs
+++ AstroPhotographyAPI/Controllers/AstroPhotographyAPIController.cs
@@ -159,5 +159,116 @@ namespace AstroPhotographyAPI.Controllers
 
             return Ok(jsonString);
         }
+
+        [EnableCors("AllowOrigin")]
+        [HttpGet("/api/v1/GenerateTransparency")]
+        public async Task<IActionResult> GenTransparency()
+        {
+            TransparencyDataJson transparencyDataJson = new TransparencyDataJson();
+            transparencyDataJson.transparecnyData = new Dictionary<string, List<TransparencyData>>();
+
+            List<TransparencyData> _data = new List<TransparencyData>();
+
+            DateTime time = DateTime.Now;
+            string generatedString = time.Year.ToString() + time.Month.ToString() + time.Day.ToString() + "12";
+
+            // convert to UTC before passing, so if you want 5PM (1700) (+8 then -12) subtract 4 so 13
+            var day = time.Day;
+            var timeToPrint = time.Hour;
+
+            var inc = (time.Hour - 4);
+
+            for (int i = inc; i < 49; i++)
+            {
+                string timeToGet = i.ToString();
+
+                if (i < 10)
+                {
+                    timeToGet = "0" + i.ToString();
+                }
+
+                var anzaColor = TransparencyData.GetAnzaRGBA(generatedString, timeToGet);
+                var transparecnyCloudCover = TransparencyData.TransparencyCoverData(anzaColor);
+
+                if ((i + 4) == 24 || (i + 4) == 48)
+                {
+                    day++;
+                    timeToPrint = timeToPrint - 24;
+                }
+
+                _data.Add(new TransparencyData()
+                {
+                    Day = day,
+                    Month = time.Month,
+                    Year = time.Year,
+                    Hour = timeToPrint,
+                    TransparencyCover = transparecnyCloudCover.Item2,
+                    RGBa = transparecnyCloudCover.Item1
+                });
+
+                if (transparencyDataJson.transparecnyData.ContainsKey(time.Year + "/" + time.Month + "/" + day))
+                {
+                    // item exists already
+                    foreach (var item in transparencyDataJson.transparecnyData)
+                    {
+                        if (item.Key == (time.Year + "/" + time.Month + "/" + day))
+                        {
+                            item.Value.Add(new TransparencyData()
+                            {
+                                Day = day,
+                                Month = time.Month,
+                                Year = time.Year,
+                                Hour = timeToPrint,
+                                TransparencyCover = transparecnyCloudCover.Item2,
+                                RGBa = transparecnyCloudCover.Item1
+                            });
+                        }
+                    }
+                }
+                else
+                {
+                    // create new key pair
+                    List<TransparencyData> newItem = new List<TransparencyData>();
+                    newItem.Add(new TransparencyData()
+                    {
+                        Day = day,
+                        Month = time.Month,
+                        Year = time.Year,
+                        Hour = timeToPrint,
+                        TransparencyCover = transparecnyCloudCover.Item2,
+                        RGBa = transparecnyCloudCover.Item1
+                    });
+
+                    transparencyDataJson.transparecnyData.Add((time.Year + "/" + time.Month + "/" + day), newItem);
+
+
+                }
+                //Console.WriteLine(time.Month.ToString() + "/" + day + "/" + time.Year + " Time: " + timeToPrint + ":00" + " " + cloudCover.Item2);
+                timeToPrint++;
+            }
+
+            string json = JsonConvert.SerializeObject(transparencyDataJson.transparecnyData, Formatting.Indented);
+
+            // write string to file
+            System.IO.File.WriteAllText("transparencyData.json", json);
+
+            List<string> listOfData = new List<string>();
+
+            foreach (var item in _data)
+            {
+                listOfData.Add(item.Month.ToString() + "/" + item.Day.ToString() + "/" + item.Year.ToString() + " - " + "Time: " + item.Hour + " - " + item.TransparencyCover);
+            }
+
+            return Ok(transparencyDataJson.transparecnyData);
+        }
+
+        [EnableCors("AllowOrigin")]
+        [HttpGet("/api/v1/GetTransparencyData")]
+        public async Task<IActionResult> GetTranspData()
+        {
+            string jsonString = System.IO.File.ReadAllText("transparencyData.json");
+
+            return Ok(jsonString);
+        }
     }
 }
