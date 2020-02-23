40d75ef1698f9d1b1afa6ece9e1fa781171aa2ea
Michael Hayes <mhayes@blizzard.com>
2020-02-22 21:39:23 -0800

    Pushing

---
+++ published-app/backupDB.ps1
@@ -0,0 +1,2 @@
+Copy-Item -Path "Resources" -Destination "C:\Users\Administrator\Desktop\dbbackup" -Recurse
+Copy-Item -Path "Photos.db" -Destination "C:\Users\Administrator\Desktop\dbbackup" -Recurse
